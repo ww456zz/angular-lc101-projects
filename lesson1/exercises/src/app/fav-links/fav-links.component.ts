bfd1515dfa22c71ec1f5b10d0e0121b884cb2731
ww456zz <ww456zzdd@gmail.com>
2022-08-17 13:21:39 -0500

    add links

---
+++ lesson1/exercises/src/app/fav-links/fav-links.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fav-links',
+  templateUrl: './fav-links.component.html',
+  styleUrls: ['./fav-links.component.css']
+})
+export class FavLinksComponent implements OnInit {
+  favLinks = ['https://www.youtube.com', 'https://www.bestbuy.com']
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
