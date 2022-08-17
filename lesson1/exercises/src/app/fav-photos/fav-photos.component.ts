1dee7c880b252e1ebbc687338935685e8e9debca
ww456zz <ww456zzdd@gmail.com>
2022-08-17 12:53:53 -0500

    first editing

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Nice View';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://unsplash.com/photos/75_s8iWHKLs';
+  image3 = '"C:\Users\Laptop1\Desktop\homework\js-assignment-4-html-me-something-ww456zz\pics\dog.png"';
 
   constructor() { }
 
