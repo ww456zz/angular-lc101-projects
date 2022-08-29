3a83a53d753611ecd520c0a076250bccaaa3ab5e
ww456zz <ww456zzdd@gmail.com>
2022-08-29 18:01:18 -0500

    final

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,68 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+  takeOffEnabled = true;
+
+  handleTakeOff() {
+    let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
+    if (result) {
+       this.color = 'blue';
+       this.height = 10000;
+       this.width = 0;
+       this.message = 'Shuttle in flight.';
+       this.takeOffEnabled=false;
+    }
+ }
+
+ handleLand(rocketImage) {
+  window.alert('The shuttle is landing. Landing gear engaged.');
+  this.color = 'green';
+  this.height = 0;
+  this.width = 0;
+  this.message = 'Shuttle landed.';
+  this.takeOffEnabled = true;
+  rocketImage.style.bottom = '0px';
+  rocketImage.style.left = '0px';
+}
+
+handleAbort(rocketImage) {
+  let result = window.confirm('Are you sure you want to end the mission?');
+  if (result) {
+    this.color = 'red';
+    this.height = 0;
+    this.width = 0;
+    this.message = 'Mission aborted.';
+    this.takeOffEnabled = true;
+    rocketImage.style.bottom = '0px';
+    rocketImage.style.left = '0px';
+  }
+}
+
+edge(width, height) {
+  if (width >200000 || width <10 || height>200000 || height<10) {
+    this.color='orange';
+    } else { this.color = 'blue';}
+}
+moveRocket(rocketImage, direction) {
+  if (direction === 'right') {
+     let movement = parseInt(rocketImage.style.left) + 10 + 'px';
+     rocketImage.style.left = movement;
+     this.width = this.width + 10000;
+  } else if (direction === 'left') {
+    let movement = parseInt(rocketImage.style.left) - 10 + 'px';
+    rocketImage.style.left = movement;
+    this.width= this.width - 10000;
+  } else if (direction === 'down') {
+    let movement = parseInt(rocketImage.style.bottom) - 10 + 'px';
+    rocketImage.style.bottom = movement;
+    this.height = this.height - 10000;
+  } else if (direction === 'up'){
+    let movement = parseInt(rocketImage.style.bottom) + 10 + 'px';
+    rocketImage.style.bottom = movement;
+    this.height = this.height + 10000;
+  }
+  this.edge(this.width, this.height);
+  
+
+}
 }
